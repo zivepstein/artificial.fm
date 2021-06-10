2ff234ac3aab201981be13156d1ce03c13f5506c
Zivvy Epstein <ziv.e@ziv.lan>
2021-06-10 12:09:46 -0400

    dumped

--- dump_songs.py
+++ dump_songs.py
@@ -29,32 +29,17 @@ if args.src == 'afm_songs':
 	    password=rds_password)
 	query = "select * from afm_songs_prod"
 	dat = sqlio.read_sql_query(query, conn)
-	for i,r in dat.iterrows():
-		local_url = '/home/host/myproject/static/music/batch3/{}.wav'.format(r['song_id'])
-		urllib.request.urlretrieve(r['url'], local_url) #todo: name songs as song_id instead of url
-		orig, sr = librosa.load(local_url, duration=50.0)
-		out = orig.copy()
-		apply_fadeout(out, sr, duration=args.fadeout)
-		#soundfile.write('original.wav', orig, samplerate=sr)
-		soundfile.write(local_url, out, samplerate=sr)
-		print("dumped and trimmed {}/{}: {}".format(i, dat.shape[0], r['song_id']))
-
-elif args.src == "sheets":
-	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-	creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
-	client = gspread.authorize(creds)
-	sheet = client.open('jukebox pipeline')
-	sheet_instance = sheet.get_worksheet(1)
-	records_data = sheet_instance.get_all_records()
-	records_df = pd.DataFrame.from_dict(records_data)
-
-	records_df = records_df.sample(frac=1).sort_values(['item'])
-	songs = list(records_df['url'].values)
-
-	for song in songs:
-		try:
-			urllib.request.urlretrieve(song, '/home/host/myproject/static/music/batch1/{}_{}'.format(song.split("/")[-3], song.split('/')[-1]))
-		except:
-			print("failed for song {}".format(song))
+elif args.src == "csv":
+	dat = pd.read_csv("path/to/afm_songs_prod.csv")
 else:
 	print("{} not supported".format(args.src))
+for i,r in dat.iterrows():
+	local_url = '/home/host/myproject/static/music/batch3/{}.wav'.format(r['song_id'])
+	urllib.request.urlretrieve(r['url'], local_url) #todo: name songs as song_id instead of url
+	orig, sr = librosa.load(local_url, duration=50.0)
+	out = orig.copy()
+	apply_fadeout(out, sr, duration=args.fadeout)
+	#soundfile.write('original.wav', orig, samplerate=sr)
+	soundfile.write(local_url, out, samplerate=sr)
+	print("dumped and trimmed {}/{}: {}".format(i, dat.shape[0], r['song_id']))
+
